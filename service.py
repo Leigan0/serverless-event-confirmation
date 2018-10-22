605f6c160b69ea0758e0a540684c3585e33149cf
Leighann.Gant <leighann.gant@FT-MW5124.local>
2018-10-22 20:58:20 +0100

    basic flask app set up with routes, linked to dynamodb

---
+++ service.py
@@ -0,0 +1,33 @@
+import boto3
+from flask import Flask, jsonify, request
+import uuid 
+
+app = Flask(__name__)
+
+# Initialize dynamodb access
+boto3.setup_default_session(profile_name='social')
+dynamodb = boto3.resource('dynamodb')
+db = dynamodb.Table('social-attendees-dev')
+
+@app.route('/attendees', methods=['GET'])
+def attendees_get():
+  res = db.scan(
+        Select='ALL_ATTRIBUTES'
+  )
+  return jsonify({'attendees': res['Items']})
+
+@app.route('/attendees/new', methods=['POST'])
+def attendee_new():
+    name = request.args.get('name')
+    gf = request.args.get('gf')
+    uuidGen = str(uuid.uuid4())
+    db.put_item(
+        Item={
+            'id': uuidGen,
+            'name': name,
+            "GF": gf,
+            "team":'2'
+        }
+    )
+    print("Written data into dynamoDB")
+    return jsonify({'status': 'added'})
