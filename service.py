e64f3e7401965c9f1140fdffe834211fd10e5807
Leighann.Gant <leighann.gant@FT-MW5124.local>
2018-10-23 22:25:11 +0100

    teams allocated randomly to players

--- service.py
+++ service.py
@@ -14,20 +14,43 @@ def attendees_get():
   res = db.scan(
         Select='ALL_ATTRIBUTES'
   )
+  print('res.items')
+  print(len(res['Items']))
   return jsonify({'attendees': res['Items']})
 
 @app.route('/attendees/new', methods=['POST'])
 def attendee_new():
     name = request.args.get('name')
-    gf = request.args.get('gf')
+    diet = request.args.get('diet')
     uuidGen = str(uuid.uuid4())
     db.put_item(
         Item={
             'id': uuidGen,
             'name': name,
-            "GF": gf,
-            "team":'2'
+            "team":team_selection(),
+            "diet": diet
         }
     )
     print("Written data into dynamoDB")
     return jsonify({'status': 'added'})
+
+def count_attendees():
+    res = db.scan(
+        Select='ALL_ATTRIBUTES'
+        )  
+    print(len(res['Items']))
+    return(len(res['Items']))
+
+def team_selection():
+    guestCount = count_attendees()
+    print("db.count")
+    print(db.item_count)
+    teams = ["Double Flop", "Raging Bulls Eye", "Dart Vaders", "Smells Like Bulls Hit"]
+    if guestCount % 4 == 0: 
+        return teams[3]
+    elif guestCount % 3 == 0:
+        return teams[2]
+    elif guestCount % 2 == 0:
+        return teams[1]
+    else:
+        return teams[0]
